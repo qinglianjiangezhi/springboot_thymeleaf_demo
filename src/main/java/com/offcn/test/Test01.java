05fe8b7d374cdb915c64a730acf1f53f0ad8110a
offcn-java <aaa@bbb>
2021-03-22 22:17:37 +0800

    增加了一个测试方法

---
+++ src/main/java/com/offcn/test/Test01.java
@@ -0,0 +1,13 @@
+package com.offcn.test;
+
+/**
+ * @Auther: sqy
+ * @Date: 2021/3/22 22:16
+ * @Description:
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
